0b58b79298ca24ac1bd3eb0831c89da5058e5152
Elsayed El-banna <111093103+Elsayed-Elpna@users.noreply.github.com>
2025-09-21 21:49:53 +0300

    Update Music.js

--- src/Music.js
+++ src/Music.js
@@ -1,12 +1,12 @@
 const tracks = [
-  { name: "Track 1", src: "../assets/sounds/1.mp3" },
-  { name: "Track 2", src: "../assets/sounds/2.mp3" },
-  { name: "Track 3", src: "../assets/sounds/3.mp3" },
-  { name: "Track 4", src: "../assets/sounds/4.mp3" },
-  { name: "Track 5", src: "../assets/sounds/5.mp3" },
-  { name: "Track 6", src: "../assets/sounds/6.mp3" },
-  { name: "Track 7", src: "../assets/sounds/7.mp3" },
-  { name: "Track 8", src: "../assets/sounds/8.mp3" },
+  { name: "Track 1", src: "/assets/sounds/1.mp3" },
+  { name: "Track 2", src: "/assets/sounds/2.mp3" },
+  { name: "Track 3", src: "/assets/sounds/3.mp3" },
+  { name: "Track 4", src: "/assets/sounds/4.mp3" },
+  { name: "Track 5", src: "/assets/sounds/5.mp3" },
+  { name: "Track 6", src: "/assets/sounds/6.mp3" },
+  { name: "Track 7", src: "/assets/sounds/7.mp3" },
+  { name: "Track 8", src: "/assets/sounds/8.mp3" },
 ];
 
 let currentTrack = 0;
@@ -39,7 +39,7 @@ function muteMusic() {
 
 // Hit sound effect
 
-const hitSound = new Audio("../assets/sounds/hiteffect.wav");
+const hitSound = new Audio("/assets/sounds/hiteffect.wav");
 function playHitSound() {
   hitSound.currentTime = 0;
   hitSound.play();
